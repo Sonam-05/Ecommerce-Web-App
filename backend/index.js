9bcb4a63dfa47ffe0aa260a981dfcc0e0d792b9f
Sonam Dangi <87596690+Sonam-05@users.noreply.github.com>
2026-01-09 12:38:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -79,9 +79,9 @@ app.use((err, req, res, next) => {
 });
 
 // static files
-app.use(express.static(path.join(__dirname, './frontend/build')))
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './frontend/build/index.html'))
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'))
 })
 
 const PORT = process.env.PORT || 5002;
