87985eb402af1d0bd7d11d14d9c19b1006b6c49c
Sonam Dangi <87596690+Sonam-05@users.noreply.github.com>
2026-01-09 11:36:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5002',
+        target: 'http://sonam-ecommerce-backend',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://localhost:5002',
+        target: 'http://sonam-ecommerce-backend',
         changeOrigin: true
       }
     }
