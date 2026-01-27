2ac02542ff53345c5b6470b9d1e3cee95af7eea1
Sonam Dangi <87596690+Sonam-05@users.noreply.github.com>
2026-01-27 09:11:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://sonam-ecommerce-backend.vercel.app',
+        target: 'https://sonam-ecommerce-frontend1.vercel.app',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://sonam-ecommerce-backend.vercel.app',
+        target: 'https://sonam-ecommerce-frontend1.vercel.app',
         changeOrigin: true
       }
     }
