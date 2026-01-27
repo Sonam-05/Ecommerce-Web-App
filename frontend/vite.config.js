30557f6a7fc97b7114021c60f5caf2cbed3ea464
Sonam Dangi <87596690+Sonam-05@users.noreply.github.com>
2026-01-27 09:17:41 +0530

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://sonam-ecommerce-frontend1.vercel.app',
+        target: 'http://sonam-ecommerce-frontend1.vercel.app',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'https://sonam-ecommerce-frontend1.vercel.app',
+        target: 'http://sonam-ecommerce-frontend1.vercel.app',
         changeOrigin: true
       }
     }
