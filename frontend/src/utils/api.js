e808c119805845193a500c2c158f2b3f97e32a74
Sonam Dangi <87596690+Sonam-05@users.noreply.github.com>
2026-02-04 15:44:01 +0530

    Add files via upload

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5002/api',
+    baseURL: 'https://sonam-ecommerce-frontend.vercel.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
